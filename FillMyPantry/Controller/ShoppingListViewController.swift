0478f9f39460b0b949be75465729c2e3cb2100a4
BINDU ELAKURTHI <e.bindu5@gmail.com>
2018-07-24 21:02:49 -0700

    Deleted Add an Item Cell from tableview

--- FillMyPantry/Controller/ShoppingListViewController.swift
+++ FillMyPantry/Controller/ShoppingListViewController.swift
@@ -74,22 +74,19 @@ class ShoppingListViewController : UIViewController, UITableViewDelegate,UITable
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if let shoppingListValue = shoppingList {
-            return (shoppingListValue.items?.count)! + 2
+            return (shoppingListValue.items?.count)! + 1
         }else{
-            return 2
+            return 1
         }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        if indexPath.row == uncompletedItems.count { // Add an Item
-            let cell =  tableView.dequeueReusableCell(withIdentifier: "createShoppingListCell", for: indexPath)
-            return cell
-        } else if indexPath.row < uncompletedItems.count { // Uncompleted List Items
+
+        if indexPath.row < uncompletedItems.count { // Uncompleted List Items
             let cell =  tableView.dequeueReusableCell(withIdentifier: "shoppingItemCell", for: indexPath) as! ShoppingListItemCell
             cell.itemLabel?.text = uncompletedItems[indexPath.row].name
             return cell
-        } else if indexPath.row == uncompletedItems.count  + 1 { // Show Hide button
+        } else if indexPath.row == uncompletedItems.count { // Show Hide button
             let cell =  tableView.dequeueReusableCell(withIdentifier: "showHideButtonCell", for: indexPath) as! ShoppingListItemCell
             if Constants.showCompletedItems {
                 cell.labeltoShowHide.text =  "Hide Completed Items"
@@ -100,7 +97,7 @@ class ShoppingListViewController : UIViewController, UITableViewDelegate,UITable
             return cell
         }else {               // Completed List Items
             let cell =  tableView.dequeueReusableCell(withIdentifier: "completedItemCell", for: indexPath) as! ShoppingListItemCell
-            cell.itemLabel?.text = completedItems[indexPath.row - uncompletedItems.count - 2].name
+            cell.itemLabel?.text = completedItems[indexPath.row - uncompletedItems.count - 1].name
             cell.checkBox.isEnabled = false
             if Constants.showCompletedItems {
                 cell.isHidden = false
@@ -116,7 +113,7 @@ class ShoppingListViewController : UIViewController, UITableViewDelegate,UITable
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
-        if indexPath.row == uncompletedItems.count + 1 { // Show Hide button
+        if indexPath.row == uncompletedItems.count { // Show Hide button
             
             let cell = tableView.cellForRow(at: indexPath) as! ShoppingListItemCell
             if (cell.labeltoShowHide.text?.contains("Show"))! {
@@ -126,17 +123,14 @@ class ShoppingListViewController : UIViewController, UITableViewDelegate,UITable
                 cell.labeltoShowHide.text =  "Show Completed Items"
                 Constants.showCompletedItems = false
             }
-        } else if indexPath.row == uncompletedItems.count { // Add an Item
-            
-            
         } else if indexPath.row < uncompletedItems.count { // Uncompleted List Items
             FirebaseDAO.updateShoppingListItem(uncompletedItems[indexPath.row].id, true)
             completedItems.append(uncompletedItems[indexPath.row])
             uncompletedItems.remove(at: indexPath.row)
         } else {      // Completed List Items
-            FirebaseDAO.updateShoppingListItem(completedItems[indexPath.row - uncompletedItems.count - 2].id, false)
-            uncompletedItems.append(completedItems[indexPath.row - uncompletedItems.count - 2])
-            completedItems.remove(at: indexPath.row - uncompletedItems.count - 1)
+            FirebaseDAO.updateShoppingListItem(completedItems[indexPath.row - uncompletedItems.count - 1].id, false)
+            uncompletedItems.append(completedItems[indexPath.row - uncompletedItems.count - 1])
+            completedItems.remove(at: indexPath.row - uncompletedItems.count )
         }
         tableView.reloadData()
     }
