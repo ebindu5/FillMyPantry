c1a1b172fc4b8e3cc0e686823d2f6f1291752733
BINDU ELAKURTHI <e.bindu5@gmail.com>
2018-07-26 23:32:21 -0700

    Expandable sections for categories Table

--- FillMyPantry/Controller/CategoriesTabViewController.swift
+++ FillMyPantry/Controller/CategoriesTabViewController.swift
@@ -9,7 +9,22 @@
 import Foundation
 import UIKit
 
-class  CategoriesTabViewController : UITableViewController{
+
+class  CategoriesTabViewController : UITableViewController,CollapsibleTableViewHeaderDelegate{
+
+    struct Section {
+        var name: String
+        var collapsed: Bool
+        
+        init(name: String, collapsed: Bool = false) {
+            self.name = name
+            self.collapsed = collapsed
+        }
+    }
+    
+    var sections = [Section]()
+    
+    
    
     var customTabController : CustomTabBarController!
     var shoppingListId : String!
@@ -21,12 +36,10 @@ class  CategoriesTabViewController : UITableViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         customTabController = self.tabBarController as? CustomTabBarController
-        
-        
-        
-        self.expandData.append(["isOpen":"1","data":["banana","mango","apple"]])
-        self.expandData.append(["isOpen":"1","data":["banana"]])
-        
+
+        // Auto resizing the height of the cell
+        tableView.estimatedRowHeight = 44.0
+        tableView.rowHeight = UITableViewAutomaticDimension
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -53,7 +66,8 @@ class  CategoriesTabViewController : UITableViewController{
         }
         
         for i in 0..<grocerySection.count {
-            self.expandData.append(["isOpen":"1","data":groceryCatalog[i]])
+           let sec = Section(name: grocerySection[i], collapsed: false)
+            sections.append(sec)
         }
 
        
@@ -66,15 +80,14 @@ class  CategoriesTabViewController : UITableViewController{
         
     }
     
+     func tableView(_ tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if self.expandData[section].value(forKey: "isOpen") as! String == "1"{
-            return 0
-        }else{
-            return groceryCatalog[section].count
-        }
         
-   
+        return sections[section].collapsed ? 0 : groceryCatalog[section].count
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -82,30 +95,55 @@ class  CategoriesTabViewController : UITableViewController{
         return grocerySection.count
     }
     
-   
-    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView?
-    {
-        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: 40))
-        headerView.backgroundColor = UIColor.green
-        let label = UILabel(frame: CGRect(x: 5, y: 3, width: headerView.frame.size.width - 1, height: 20))
-        label.text = "\(grocerySection[section])"
-        headerView.layer.borderWidth = 0.1
-        headerView.addSubview(label)
-        headerView.tag = section + 100
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "header") as? CollapsibleTableViewHeader ?? CollapsibleTableViewHeader(reuseIdentifier: "header")
         
-        let tapgesture = UITapGestureRecognizer(target: self , action: #selector(self.sectionTapped(_:)))
-        headerView.addGestureRecognizer(tapgesture)
-        return headerView
+        header.titleLabel.text = sections[section].name
+        header.arrowLabel.text = ">"
+        header.setCollapsed(sections[section].collapsed)
+        
+        header.section = section
+        header.delegate = self
+        
+        return header
     }
     
-    @objc func sectionTapped(_ sender: UITapGestureRecognizer){
-        if(self.expandData[(sender.view?.tag)! - 100].value(forKey: "isOpen") as! String == "1"){
-            self.expandData[(sender.view?.tag)! - 100].setValue("0", forKey: "isOpen")
-        }else{
-            self.expandData[(sender.view?.tag)! - 100].setValue("1", forKey: "isOpen")
-        }
-        self.tableView.reloadSections(IndexSet(integer: (sender.view?.tag)! - 100), with: .automatic)
+    
+    func toggleSection(_ header: CollapsibleTableViewHeader, section: Int) {
+        let collapsed = !sections[section].collapsed
+        
+        // Toggle collapse
+        sections[section].collapsed = collapsed
+        header.setCollapsed(collapsed)
+        
+        // Reload the whole section
+        tableView.reloadSections(NSIndexSet(index: section) as IndexSet, with: .automatic)
     }
+    
+   
+//    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView?
+//    {
+//        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: 40))
+//        headerView.backgroundColor = UIColor.green
+//        let label = UILabel(frame: CGRect(x: 5, y: 3, width: headerView.frame.size.width - 1, height: 20))
+//        label.text = "\(grocerySection[section])"
+//        headerView.layer.borderWidth = 0.1
+//        headerView.addSubview(label)
+//        headerView.tag = section + 100
+//        
+//        let tapgesture = UITapGestureRecognizer(target: self , action: #selector(self.sectionTapped(_:)))
+//        headerView.addGestureRecognizer(tapgesture)
+//        return headerView
+//    }
+//    
+//    @objc func sectionTapped(_ sender: UITapGestureRecognizer){
+//        if(self.expandData[(sender.view?.tag)! - 100].value(forKey: "isOpen") as! String == "1"){
+//            self.expandData[(sender.view?.tag)! - 100].setValue("0", forKey: "isOpen")
+//        }else{
+//            self.expandData[(sender.view?.tag)! - 100].setValue("1", forKey: "isOpen")
+//        }
+//        self.tableView.reloadSections(IndexSet(integer: (sender.view?.tag)! - 100), with: .automatic)
+//    }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return grocerySection[section]
